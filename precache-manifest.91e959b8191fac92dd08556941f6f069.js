2833fa20cb3a861d8e0b549fbed319b68e3dc98b
Tomas <akavickistomas@Gmail.com>
2020-10-04 19:06:34 +0100

    Updates

---
+++ precache-manifest.91e959b8191fac92dd08556941f6f069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbede5732dafd7c763f66a12c817efe",
+    "url": "/word-frequency/index.html"
+  },
+  {
+    "revision": "dd2ecca69eb8657caac7",
+    "url": "/word-frequency/static/css/main.8632f022.chunk.css"
+  },
+  {
+    "revision": "f03b8cd110c610b3e5ba",
+    "url": "/word-frequency/static/js/2.460414ed.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/word-frequency/static/js/2.460414ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2ecca69eb8657caac7",
+    "url": "/word-frequency/static/js/main.ba1b47c8.chunk.js"
+  },
+  {
+    "revision": "689de112ffe5930af3be",
+    "url": "/word-frequency/static/js/runtime-main.fe40dc26.js"
+  }
+]);
